7f5269f3869750abe8beb5d587df91f30463cca4
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-16 18:35:18 +0100

    Create signup form UI + validation with formik

--- frontend/src/components/sign-up/SignUp.jsx
+++ frontend/src/components/sign-up/SignUp.jsx
@@ -1,70 +1,113 @@
 import React from "react";
 
+import { Form } from "react-router-dom";
+
 import { Button } from "../UI/Button";
 import { Input } from "../UI/Input";
 
+import { Formik } from "formik";
+import * as yup from "yup";
+
 import classes from "./SignUp.module.css";
+
+const signUpSchema = yup.object().shape({
+  firstName: yup
+    .string()
+    .matches(/^[A-Za-z]+$/, {
+      excludeEmptyString: true,
+      message: "Only letters allowed.",
+    })
+    .required("First name is required."),
+  lastName: yup
+    .string()
+    .matches(/^[A-Za-z]+$/, {
+      excludeEmptyString: true,
+      message: "Only letters allowed.",
+    })
+    .required("Last name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  userBudget: yup
+    .number()
+    .integer()
+    .required("Please inserting your starting budget"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long")
+    .matches(/[0-9]/, "Password requires a number")
+    .matches(/[a-z]/, "Password requires a lowercase letter")
+    .matches(/[A-Z]/, "Password requires an uppercase letter")
+    .matches(/[^\w]/, "Password requires a symbol"),
+});
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  userBudget: 0,
+  password: "",
+};
 
 export function SignUp() {
   return (
-    <form className={classes["form-container"]}>
-      <h1>Start budgeting for free</h1>
-      <div className={classes["flex-field"]}>
-        <Input
-          type="text"
-          id="first-name"
-          name="first-name"
-          label="First name"
-          placeholder="Mario"
-          required={true}
-        />
-        <Input
-          type="text"
-          id="last-name"
-          name="last-name"
-          label="Last name"
-          placeholder="Rossi"
-        />
-      </div>
-      <Input
-        type="email"
-        id="email"
-        name="email"
-        label="Email"
-        placeholder="mariorossi@example.com"
-        required={true}
-      />
-      <Input
-        type="number"
-        id="user-budget"
-        name="user-budget"
-        label="Your starting budget"
-        required={true}
-      />
-      <Input
-        type="password"
-        id="password"
-        name="password"
-        label="Password"
-        required={true}
-      />
-      <ul className={classes["passw-requirements"]}>
-        <div>
-          <li>A minimum of 8 character</li>
-          <li>At least one lowercase letter</li>
-        </div>
-        <div>
-          <li>At least one number</li>
-          <li>At least one upper letter</li>
-        </div>
-      </ul>
-      <Button type="submit" label="Create account" />
-      <p className={classes["sign-in"]}>
-        Have an account?{" "}
-        <a href="#" className={classes["sign-in__link"]}>
-          Sign in
-        </a>
-      </p>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={signUpSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {(props) => (
+        <Form
+          onSubmit={props.handleSubmit}
+          className={classes["form-container"]}
+        >
+          <h1>Start budgeting for free</h1>
+          <div className={classes["flex-field"]}>
+            <Input
+              label="First name"
+              name="firstName"
+              type="text"
+              placeholder="John"
+            />
+
+            <Input
+              label="Last name"
+              name="lastName"
+              type="text"
+              placeholder="Smidth"
+            />
+          </div>
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            placeholder="john@example.com"
+          />
+          <Input label="Your starting budget" name="userBudget" type="number" />
+          <Input label="Password" name="password" type="password" />
+          <ul className={classes["passw-requirements"]}>
+            <div>
+              <li>A minimum of 8 character</li>
+              <li>At least one lowercase letter</li>
+            </div>
+            <div>
+              <li>At least one number</li>
+              <li>At least one upper letter</li>
+            </div>
+          </ul>
+          {!props.isSubmitting && (
+            <Button type="submit" label="Create account" />
+          )}
+
+          <p className={classes["sign-in"]}>
+            Have an account?
+            <a href="#" className={classes["sign-in__link"]}>
+              Sign in
+            </a>
+          </p>
+        </Form>
+      )}
+    </Formik>
   );
 }
