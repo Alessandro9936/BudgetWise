8e9f81a3ef6f22eb1d231d22482c3aefd3366b94
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-16 19:18:12 +0100

    Extract input into a separate and reusable component

--- frontend/src/components/UI/Input.jsx
+++ frontend/src/components/UI/Input.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+
+import { useField } from "formik";
+
+import { AlertTriangle } from "react-feather";
 import classes from "./styles/Input.module.css";
 
-export function Input({ label, ...inputAttributes }) {
+export function Input({ label, ...props }) {
+  /*
+  useField is custom React Hook that returns a 3-tuple (an array with three elements) containing FieldProps, FieldMetaProps and FieldHelperProps. It accepts either a string of a field name or an object as an argument. The object must at least contain a name key.
+
+  FieldProps will inject onChange, onBlur, name, and value props of the field designated by the name attribute.
+
+  FieldMetaProps contains computed values (error, touched, ...) about the field which can be used to style the field
+  */
+
+  const [field, meta] = useField(props);
+
+  let inputClasses;
+
+  if (meta.touched && meta.error) {
+    inputClasses = classes["input-error"];
+  }
+
+  if ((meta.touched && !meta.error) || (!meta.touched && field.value)) {
+    inputClasses = classes["input-valid"];
+  }
+
   return (
     <div>
-      <label htmlFor={inputAttributes.id}>{label}:</label>
-      <input {...inputAttributes} />
+      <label htmlFor={props.name}>{label}</label>
+      <input {...field} {...props} className={inputClasses} />
+      {meta.touched && meta.error ? (
+        <div className={classes["error-message"]}>
+          <AlertTriangle size={14} />
+          <p>{meta.error}</p>
+        </div>
+      ) : null}
     </div>
   );
 }
