a7c847e0bc50354144951cdd08876b1bbb5b5723
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-18 18:39:05 +0100

    Create user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const schema = mongoose.Schema;
+
+const { JWT_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
 const userSchema = new schema({
   firstName: { type: String, required: true },
