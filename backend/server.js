ff75c7daf4c95c75750b625ce2aa393aa2993137
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-23 18:24:37 +0100

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Initiate routes
 app.use("/", userRoutes);
