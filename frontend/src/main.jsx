0b4408bf2c87d15dc6b8716011538a8b8b95d49b
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-24 19:13:43 +0100

    Remove App component and set content back into main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import { SignUp } from "./pages/SignUp/SignUp";
+import { Login } from "./pages/Login/Login";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
