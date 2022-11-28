eaebbb6d3b29786c2d7572e9e12593b3c21bca9c
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-28 14:16:36 +0100

    Create summary component

---
+++ frontend/src/pages/Dashboard/components/Summary.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Card from "../../../components/UI/Card";
+import classes from "./Summary.module.css";
+
+import { transactionMapped } from "../../../../data/data";
+
+export function Summary() {
+  const amounts = transactionMapped.reduce((acc, cur) => {
+    if (cur.type === "income" && !acc.income) {
+      acc.income = cur.amount;
+    } else {
+      acc.income += cur.amount;
+    }
+
+    if (cur.type === "expense" && !acc.expenses) {
+      acc.expenses = cur.amount;
+    } else {
+      acc.expenses += cur.amount;
+    }
+
+    return acc;
+  }, {});
+
+  const totalBalance = amounts.income - amounts.expenses;
+
+  return (
+    <section className={classes.summary}>
+      <h3>Overview</h3>
+      <Card>
+        <p className={classes["summary-field__text"]}>Income</p>
+        <p className={classes["summary-amount"]}>$ {amounts?.income}</p>
+      </Card>
+      <Card>
+        <p className={classes["summary-field__text"]}>Expenses</p>
+        <p className={classes["summary-amount"]}>$ {amounts?.expenses}</p>
+      </Card>
+      <Card>
+        <p className={classes["summary-field__text"]}>Total Balance</p>
+        <p
+          className={`${classes["summary-amount"]} ${
+            totalBalance > 0 ? classes.positive : classes.negative
+          }`}
+        >
+          $ {totalBalance}
+        </p>
+      </Card>
+    </section>
+  );
+}
