0b5d607bea5424ff70541e9b6c2aa3fa73e1a575
Alessandro Crinella <alecrinella99@gmail.com>
2022-11-30 21:18:59 +0100

    set cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Initiate routes
 app.use("/", userRoutes);
