93801a7ebcd0ba9253a4c08bf35588c9c2a8bec0
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-02 18:46:59 +0100

    Create sub component HeaderState of Invoices

---
+++ frontend/src/pages/Invoices/components/HeaderStates.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import classes from "./HeaderStates.module.css";
+
+import Card from "../../../components/UI/Card";
+import { FileText } from "react-feather";
+
+import { transactionMapped } from "../../../../data/data";
+
+export function HeaderStates() {
+  const statesNumber = transactionMapped
+    .filter((transaction) => transaction.type === "expense")
+    .reduce(
+      (acc, cur) => {
+        acc[cur.state]++;
+        return acc;
+      },
+      { Paid: 0, "To pay": 0, Upcoming: 0 }
+    );
+
+  console.log(statesNumber);
+
+  return (
+    <section className={classes["header-states"]}>
+      <Card>
+        <div className={classes["state-container"]}>
+          <FileText
+            size={50}
+            style={{
+              backgroundColor: "#95cba0",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          />
+          <div className={classes["state-info"]}>
+            <p>{statesNumber.Paid}</p>
+            <p>Paid</p>
+          </div>
+        </div>
+        <div className={classes["state-container"]}>
+          <FileText
+            size={50}
+            style={{
+              backgroundColor: "#ee7172",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          />
+          <div className={classes["state-info"]}>
+            <p>{statesNumber["To pay"]}</p>
+            <p>To pay</p>
+          </div>
+        </div>
+        <div className={classes["state-container"]}>
+          <FileText
+            size={50}
+            style={{
+              backgroundColor: "#faaa4c",
+              padding: "5px",
+              borderRadius: "10px",
+            }}
+          />
+          <div className={classes["state-info"]}>
+            <p>{statesNumber.Upcoming}</p>
+            <p>Upcoming</p>
+          </div>
+        </div>
+      </Card>
+    </section>
+  );
+}
