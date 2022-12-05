aa47f1127b98a02a9ababa47792a22051b67a6df
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-05 21:30:01 +0100

    Create Budgets component

--- frontend/src/pages/Budgets/Budgets.jsx
+++ frontend/src/pages/Budgets/Budgets.jsx
@@ -1,3 +1,48 @@
+import { BudgetChart } from "./components/BudgetChart";
+import { BudgetPreviews } from "./components/BudgetPreviews";
+import { Bar } from "./components/Bar";
+import { ContentGrid } from "../../components/UI/ContentGrid";
+import classes from "./Budgets.module.css";
+
+import { useActiveDates } from "../hooks/useActiveDates";
+import { TimeSpanSelector } from "../../components/UI/TimeSpanSelector";
+import { DateBar } from "../../components/UI/DateBar";
+
+import { budgets } from "../../../data/data";
+import { isSameMonth, isSameYear } from "date-fns";
+
 export default function Budgets() {
-  return <div>Budgets</div>;
+  const {
+    updateActiveDate,
+    updateActiveTimeSpan,
+    activeDate,
+    activeTimeSpan,
+    activeDateFormatted,
+  } = useActiveDates();
+
+  const activeBudgets =
+    activeTimeSpan === "Yearly"
+      ? budgets.filter((budget) => isSameYear(budget.month, activeDate))
+      : budgets.filter((budget) => isSameMonth(budget.month, activeDate));
+
+  return (
+    <ContentGrid gridAreas={classes["budget-areas"]}>
+      <Bar>
+        <TimeSpanSelector
+          timeSpans={["Yearly", "Monthly"]}
+          updateActiveTimeSpan={updateActiveTimeSpan}
+          activeTimeSpan={activeTimeSpan}
+        />
+        <DateBar
+          updateActiveDate={updateActiveDate}
+          activeDate={activeDateFormatted}
+        />
+      </Bar>
+      <BudgetPreviews />
+      <BudgetChart
+        activeBudgets={activeBudgets}
+        activeTimeSpan={activeTimeSpan}
+      />
+    </ContentGrid>
+  );
 }
