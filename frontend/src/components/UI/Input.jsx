a1b5bdeb99d2ec4a281598bf33b48eda79b85aa2
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-08 21:05:05 +0100

    Update reusable text input for forms

--- frontend/src/components/UI/Input.jsx
+++ frontend/src/components/UI/Input.jsx
@@ -1,38 +1,21 @@
 import React from "react";
 
-import { useField } from "formik";
+import { ErrorMessage, Field } from "formik";
 
 import { InputError } from "./InputError";
 import classes from "../styles/Input.module.css";
 
-export function Input({ label, ...props }) {
-  /*
-  useField is custom React Hook that returns a 3-tuple (an array with three elements) containing FieldProps, FieldMetaProps and FieldHelperProps. It accepts either a string of a field name or an object as an argument. The object must at least contain a name key.
-
-  FieldProps will inject onChange, onBlur, name, and value props of the field designated by the name attribute.
-
-  FieldMetaProps contains computed values (error, touched, ...) about the field which can be used to style the field
-  */
-
-  const [field, meta] = useField(props);
-
-  let inputClasses;
-
-  if (field.value && !meta.error && meta.touched) {
-    inputClasses = classes["input-valid"];
-  }
-
-  if (meta.touched && meta.error) {
-    inputClasses = classes["input-error"];
-  }
-
+export function Input({ label, name, ...props }) {
   return (
     <div className={classes["input-field"]}>
-      <label className={classes.label} htmlFor={props.name}>
+      <label className={classes.label} htmlFor={name}>
         {label}
       </label>
-      <input {...field} {...props} className={inputClasses + classes.input} />
-      {meta.touched && meta.error ? <InputError message={meta.error} /> : null}
+      <Field name={name} {...props} />
+      <ErrorMessage
+        name={name}
+        render={(msg) => <InputError message={msg} />}
+      />
     </div>
   );
 }
