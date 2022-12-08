5af6c7b2261c4bda38c0247c6d4e84ca590203b0
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-08 21:11:45 +0100

    Update modal wrapper component

--- frontend/src/components/Utilities/Modal.jsx
+++ frontend/src/components/Utilities/Modal.jsx
@@ -1,15 +1,17 @@
 import Card from "../UI/Card";
-import classes from "../styles/TransactionModal.module.css";
+import classes from "../styles/Modal.module.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Modal({ children }) {
+  const navigate = useNavigate();
   return (
     <>
       {ReactDOM.createPortal(
         <>
-          <div className={classes.overlay} />
+          <div className={classes.overlay} onClick={() => navigate("..")} />
           <div className={classes.modal}>
             <Card>{children}</Card>
           </div>
