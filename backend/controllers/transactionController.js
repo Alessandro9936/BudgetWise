51fc454353a8706142c6b65de24d123f808a0549
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-09 21:04:13 +0100

    Create transaction controllers and services

---
+++ backend/controllers/transactionController.js
@@ -0,0 +1,83 @@
+const createHttpError = require("http-errors");
+
+const {
+  userTransactionsService,
+  newTransactionService,
+  updateTransactionService,
+  getTransactionService,
+  deleteTransactionService,
+} = require("../services/transactionServices");
+
+// @desc get all transactions of logged user
+// @route GET /api/transactions
+// @access Private
+const userTransactions = async (req, res, next) => {
+  try {
+    const { id: userID } = req.user;
+    const transactions = await userTransactionsService(userID);
+    res.status(200).json(transactions);
+  } catch (error) {
+    next(createHttpError(error));
+  }
+};
+
+// @desc create new transaction
+// @route POST /api/transactions
+// @access Private
+const newTransaction = async (req, res, next) => {
+  try {
+    const transaction = await newTransactionService(req);
+    res.status(201).json(transaction);
+  } catch (error) {
+    next(createHttpError(error));
+  }
+};
+
+// @desc update transaction
+// @route PUT /api/transactions/:id
+// @access Private
+const updateTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updatedTransaction = await updateTransactionService(id, req.body);
+    res.status(201).json(updatedTransaction);
+  } catch (error) {
+    next(createHttpError(error));
+  }
+};
+
+// @desc get transaction details
+// @route GET /api/transactions/:id
+// @access Private
+const getTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const transaction = await getTransactionService(id);
+
+    res.status(200).json(transaction);
+  } catch (error) {
+    next(createHttpError(error));
+  }
+};
+
+// @desc delete transaction
+// @route DELETE /api/transactions/:id
+// @access Private
+const deleteTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await deleteTransactionService(id);
+
+    res.status(204).end();
+  } catch (error) {
+    next(createHttpError(error));
+  }
+};
+
+module.exports = {
+  userTransactions,
+  newTransaction,
+  updateTransaction,
+  getTransaction,
+  deleteTransaction,
+};
---
+++ backend/services/transactionServices.js
@@ -0,0 +1,74 @@
+const Transaction = require("../models/transactionModel");
+
+const userTransactionsService = async (userID) => {
+  try {
+    const userTransactions = await Transaction.getUserTransactions(userID);
+    return userTransactions;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const newTransactionService = async (req) => {
+  try {
+    const transaction = new Transaction({
+      user: req.user._id,
+      type: req.body.type,
+      amount: req.body.amount,
+      date: req.body.date,
+      description: req.body.description,
+    });
+
+    if (transaction.type === "expense") {
+      transaction.budget = req.body.budget;
+      transaction.state = req.body.state;
+    }
+
+    await transaction.save();
+    return transaction;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updateTransactionService = async (id, body) => {
+  try {
+    const oldTransaction = await Transaction.findById(id);
+
+    for ([key, value] of Object.entries(body)) {
+      if (oldTransaction[key] !== body[key]) {
+        oldTransaction[key] = value;
+      }
+    }
+
+    const updatedTransaction = oldTransaction.save();
+    return updatedTransaction;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getTransactionService = async (id) => {
+  try {
+    const transaction = await Transaction.findById(id);
+    return transaction;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const deleteTransactionService = async (id) => {
+  try {
+    await Transaction.findByIdAndRemove(id);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  userTransactionsService,
+  newTransactionService,
+  updateTransactionService,
+  getTransactionService,
+  deleteTransactionService,
+};
