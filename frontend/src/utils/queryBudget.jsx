208dac331b3a4c85fd6c15eb09796df58b32cf6f
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-20 22:15:45 +0100

    Update query to retrieve budgets

--- frontend/src/utils/queryBudget.jsx
+++ frontend/src/utils/queryBudget.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "react-query";
 import { useSearchParams } from "react-router-dom";
 import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
 
-export const useGetBudgets = () => {
+export const useGetBudgets = (defaultDate) => {
   const axiosPrivate = useAxiosPrivate();
   const [search] = useSearchParams();
 
+  const searchString = search.get("date") || defaultDate;
+
   return useQuery(
-    ["budgets", search.toString()],
-    () => axiosPrivate.get("/api/budgets", { params: search }),
+    ["budgets", searchString],
+    () => axiosPrivate.get("/api/budgets", { params: { date: searchString } }),
     {
       staleTime: 12000,
       keepPreviousData: true,
