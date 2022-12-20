77da9b6b3bcfa0d305d305756483d3d2b1a2c60a
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-20 22:08:02 +0100

    Remove queries for budgets and transactions from App component

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -1,14 +1,8 @@
-import classes from "./App.module.css";
-
 import Header from "../../layouts/Header";
 import Sidebar from "../../layouts/Sidebar";
 import { Outlet } from "react-router-dom";
-import { useGetTransactions } from "../../utils/queryTransactions";
-import { useGetBudgets } from "../../utils/queryBudget";
 
 export default function App() {
-  useGetTransactions();
-  useGetBudgets();
   return (
     <>
       <Header />
