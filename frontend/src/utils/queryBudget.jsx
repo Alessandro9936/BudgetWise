eaceadc597724c29b0a3acef1de4f59387c230ef
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-21 21:41:40 +0100

    Create mutation function to generate new budget

--- frontend/src/utils/queryBudget.jsx
+++ frontend/src/utils/queryBudget.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useSearchParams } from "react-router-dom";
 import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
 
@@ -6,13 +6,13 @@ export const useGetBudgets = (defaultDate) => {
   const axiosPrivate = useAxiosPrivate();
   const [search] = useSearchParams();
 
-  const searchString = search.get("date") || defaultDate;
+  const searchValue = search.get("date") || defaultDate;
 
   return useQuery(
-    ["budgets", searchString],
-    () => axiosPrivate.get("/api/budgets", { params: { date: searchString } }),
+    ["budgets", searchValue],
+    () => axiosPrivate.get("/api/budgets", { params: { date: searchValue } }),
     {
-      staleTime: 12000,
+      staleTime: 120000,
       keepPreviousData: true,
       select: (data) =>
         data.data.map((transaction) => ({
@@ -21,4 +21,65 @@ export const useGetBudgets = (defaultDate) => {
         })),
     }
   );
+};
+
+export const useGetBudgetsByDate = (date) => {
+  const axiosPrivate = useAxiosPrivate();
+
+  return useQuery(
+    ["budgets", date],
+    () => axiosPrivate.get("/api/budgets", { params: { date } }),
+    {
+      staleTime: 120000,
+      keepPreviousData: true,
+      select: (data) =>
+        data.data.map((transaction) => ({
+          ...transaction,
+          date: new Date(transaction.date),
+        })),
+    }
+  );
+};
+
+export const budgetsByDate = (date) => {
+  const query = useGetBudgetsByDate(date);
+
+  const budgets = query.data ?? [];
+  const isLoading = query.isLoading;
+  const isFetching = query.isFetching;
+
+  return { budgets, isFetching, isLoading };
+};
+
+export const useNewBudget = (formState, setError) => {
+  const axiosPrivate = useAxiosPrivate();
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation((values) =>
+    axiosPrivate.post("/api/budgets", values)
+  );
+
+  const newBudget = (formData) => {
+    mutate(formData, {
+      onSuccess: (data) => {
+        if (data.status === 201) {
+          const budgetMonth = new Date(data.data?.date).toLocaleDateString(
+            "en-GB",
+            { month: "long", year: "numeric" }
+          );
+          const budgetYear = new Date(data.data?.date).getFullYear();
+          Promise.all(
+            queryClient.invalidateQueries(["budgets", budgetMonth]),
+            queryClient.invalidateQueries(["budgets", budgetYear])
+          );
+        }
+      },
+      onError: (error) => {
+        const { param: field, msg: message } = error.response.data[0];
+        setError(field, { message: message }, { shouldFocus: true });
+        formState.isSubmitSuccessful(false);
+      },
+    });
+  };
+  return { newBudget };
 };
