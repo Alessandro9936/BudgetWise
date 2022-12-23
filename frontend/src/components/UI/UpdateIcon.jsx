bec4973973e883d5518025440594bd26bd7d79cb
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-23 10:39:49 +0100

    Update redirect links

--- frontend/src/components/UI/UpdateIcon.jsx
+++ frontend/src/components/UI/UpdateIcon.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Edit } from "react-feather";
 import { Link } from "react-router-dom";
-export function UpdateIcon({ id }) {
+export function UpdateIcon({ redirectLink, activeTimeSpan }) {
   return (
-    <Link to={`${id}`} style={{ display: "inline-block", height: "18px" }}>
+    <Link
+      to={redirectLink}
+      state={{ activeTimeSpan }}
+      style={{ display: "inline-block", height: "18px" }}
+    >
       <Edit size={18} color={"#929292"} cursor={"pointer"} />
     </Link>
   );
