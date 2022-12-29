da6482c8a1562ef579e941972a2f2f95e4372c5e
Alessandro Crinella <alecrinella99@gmail.com>
2022-12-29 18:15:46 +0100

    Create custom hook on axios instance
    This custom hook allow to verify the availability of the access token to
    make request to the server. If there's no access token a new one
    is generated if refresh token has not expired

---
+++ frontend2/src/hooks/useAxiosPrivate.tsx
@@ -0,0 +1,62 @@
+import axios, { AxiosError } from "axios";
+import { useEffect } from "react";
+import { getAccessToken } from "../services/accessTokenHandler";
+import useRefreshToken from "./useRefreshToken";
+
+const axiosPrivate = axios.create({
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+const useAxiosPrivate = () => {
+  const refresh = useRefreshToken();
+  const accessToken = getAccessToken();
+
+  useEffect(() => {
+    /*
+      Right after the user login the generated access token will not be automatically
+      attached to the header. This request interceptor handle the attachment of the
+      access token to the header on first call after login
+      */
+    const requestIntercept = axiosPrivate.interceptors.request.use((config) => {
+      config.headers = config.headers ?? {};
+      if (!config.headers["Authorization"]) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+      }
+      return config;
+    });
+
+    /*
+      Access tokens have a short life span (10 mins), this allows to limit the time
+      an attacker can abuse a stolen token. This also means that at a certain point, when 
+      the user is navigating, the token could be expired on the next call. This interceptor
+      attached on the response allow to handle in the background the case where the access token
+      is expired and generate a new one if the refresh token is still valid.
+      */
+    const responseIntercept = axiosPrivate.interceptors.response.use(
+      (response) => response,
+      async (error: AxiosError) => {
+        const prevRequest = error.config!;
+        let isSent = false;
+        if (error.response?.status === 401 && !isSent) {
+          /* isSent variable allow to retry to get an accessToken only once, if the
+          first call returns an error it means that also the refrsh token has expired */
+          isSent = true;
+          const newAccessToken = await refresh();
+          prevRequest.headers!["Authorization"] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosPrivate.interceptors.response.eject(responseIntercept);
+      axiosPrivate.interceptors.request.eject(requestIntercept);
+    };
+  }, [accessToken, refresh]);
+
+  return axiosPrivate;
+};
+
+export default useAxiosPrivate;
