57c300357107f4833d369cb4683b9016ce4bbf01
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-02 20:43:35 +0100

    Import sub components and complete dashboard layout

--- frontend2/src/pages/dashboard/dashboard.tsx
+++ frontend2/src/pages/dashboard/dashboard.tsx
@@ -1,21 +1,23 @@
-import Card from "../../components/card";
+import Activity from "./components/activity";
+import Calendar from "./components/calendar";
+import Graph from "./components/graph";
 
 import Summary from "./components/summary";
 
 const Dashboard = () => {
   return (
-    <section className="grid flex-1 grid-cols-1 gap-6 bg-slate-100 p-6 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2">
+    <section className="grid flex-1 grid-cols-1 gap-6 bg-slate-100 p-6 md:grid-cols-2 lg:grid-cols-[1.25fr_1fr_1.25fr] lg:grid-rows-[auto_1fr]">
       {/* Graph */}
-      <Card classNames="order-2 md:col-span-full lg:order-1 lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-4 " />
+      <Graph gridDisposition="order-2 md:col-span-full lg:order-1 lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-4 " />
 
       {/* Summary */}
       <Summary gridDisposition="order-1 md:order-3" />
 
       {/* Calendar */}
-      <Card classNames="order-3 md:order-4 " />
+      <Calendar gridDisposition="order-3 md:order-4 " />
 
       {/* Activity */}
-      <Card classNames="order-4 md:order-2 md:row-start-2 md:row-end-4 lg:col-start-3 lg:col-end-5 lg:row-start-1" />
+      <Activity gridDisposition="order-4 col-span-full lg:order-2 lg:row-start-2 lg:row-end-4 lg:col-start-3 lg:col-end-5 lg:row-start-1" />
     </section>
   );
 };
