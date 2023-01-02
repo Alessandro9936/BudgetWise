56ae3924921f2e0be976fd96f4008ed28751d22c
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-02 20:42:01 +0100

    Create reusable button to redirect to another route

---
+++ frontend2/src/components/Buttons/ButtoinRedirect.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const ButtonRedirect = ({
+  redirect,
+  styles,
+  label,
+}: {
+  redirect: string;
+  styles: string;
+  label: string;
+}) => {
+  return (
+    <Link
+      to={redirect}
+      className={`rounded-lg py-3 text-center  font-semibold  transition disabled:pointer-events-none disabled:bg-slate-200 ${styles}`}
+    >
+      {label}
+    </Link>
+  );
+};
+
+export default ButtonRedirect;
