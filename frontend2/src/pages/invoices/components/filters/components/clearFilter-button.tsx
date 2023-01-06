3880286700f6ead51d9f1a57c3571346800631b4
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-06 18:21:51 +0100

    extract reset field function from component

--- frontend2/src/pages/invoices/components/filters/components/clearFilter-button.tsx
+++ frontend2/src/pages/invoices/components/filters/components/clearFilter-button.tsx
@@ -1,24 +1,15 @@
-import { useSearchParams } from "react-router-dom";
-
 const ClearFilterButton = ({
-  filter,
   disabled,
+  reset,
 }: {
-  filter: string;
   disabled: boolean;
+  reset: () => void;
 }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const clearFilter = () => {
-    searchParams.delete(filter);
-    setSearchParams(searchParams);
-  };
-
   return (
     <button
       disabled={disabled}
       className="mt-2 w-full rounded-lg bg-slate-900 py-2 font-semibold text-white transition-all hover:bg-purple-500 disabled:pointer-events-none disabled:bg-slate-200"
-      onClick={clearFilter}
+      onClick={reset}
     >
       Clear
     </button>
