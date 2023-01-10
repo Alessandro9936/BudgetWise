7017b52840cf9a3813c4949bf1a54d599ed74c99
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-10 16:27:41 +0100

    Create wrapper that includes different state of a form
    This wrapper is utilizied in all modals inside the application. It
    allows to detect the current state of the form, and depending on it
    the right content will be displayed

---
+++ frontend2/src/components/Form/form-handler.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import SubmitButton from "../Buttons/SubmitButton";
+import ButtonRedirect from "../Buttons/ButtoinRedirect";
+
+const FormHandler = ({
+  isLoading,
+  submitLabel,
+  isSubmitSuccessful,
+}: {
+  isLoading: boolean;
+  isSubmitSuccessful: boolean;
+  submitLabel: string;
+}) => {
+  const navigate = useNavigate();
+  const [secondsRedirect, setSecondsRedirect] = useState(5);
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      const interval = setInterval(() => {
+        setSecondsRedirect((prev) => prev - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isSubmitSuccessful]);
+
+  useEffect(() => {
+    if (secondsRedirect === 0 && isSubmitSuccessful) {
+      navigate("..");
+    }
+  }, [secondsRedirect]);
+
+  return !isSubmitSuccessful ? (
+    <div className="ml-auto flex w-fit justify-end gap-x-2">
+      <SubmitButton label={submitLabel} styles="px-6" isLoading={isLoading} />
+
+      <ButtonRedirect
+        redirect={".."}
+        styles="mt-2 px-6 bg-white text-purple-500 ring-1 ring-purple-500"
+        label="Go back"
+      />
+    </div>
+  ) : (
+    <div className="mt-4 text-center">
+      <p className="font-semibold">Budget successfully created</p>
+      <p>You will be redirected in {secondsRedirect} seconds</p>
+    </div>
+  );
+};
+
+export default FormHandler;
