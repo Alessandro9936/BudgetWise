a086d6296dfb00768923cf1718eff4a8f24d1aee
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-14 22:53:35 +0100

    Update budget validation schema

--- frontend2/src/pages/budget/utils/validation-schema.tsx
+++ frontend2/src/pages/budget/utils/validation-schema.tsx
@@ -3,15 +3,13 @@ import { z } from "zod";
 
 const BudgetSchema = z.object({
   name: z.string().min(1, { message: "Budget name must be defined" }),
-  date: z
-    .date()
-    .min(startOfMonth(new Date()), {
-      message: "Budget must be set in a future date",
-    }),
+  date: z.date().min(startOfMonth(new Date()), {
+    message: "Budget must be set in a future date",
+  }),
   maxAmount: z.coerce
     .number()
     .gt(0, { message: "Maximum amount must be greater than zero" }),
-  usedAmount: z.coerce.number(),
+  usedAmount: z.coerce.number().optional().default(0),
 });
 
 export default BudgetSchema;
