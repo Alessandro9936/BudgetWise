f1a9e5477b288d43b1df2d6a053ca05754224281
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-15 22:04:42 +0100

    Implement remaining transaction routes

--- frontend2/src/services/transaction-services.tsx
+++ frontend2/src/services/transaction-services.tsx
@@ -1,6 +1,12 @@
-import { useQuery } from "react-query";
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "react-query";
 import { useParams, useSearchParams } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { ITransactionForm } from "../pages/transaction/types/types";
 import { IBudgetResponse } from "./budget-services";
 
 const transactionKeys = {
@@ -10,14 +16,13 @@ const transactionKeys = {
   detail: (id: string) => ["transaction", id] as const,
 };
 
-export interface ITransaction {
+export interface ITransactionResponse {
   type: "income" | "expense";
   amount: number;
   date: Date;
   description?: string;
   _id: string;
   user: string;
-  __v: number;
   state?: "paid" | "topay" | "upcoming";
   budget?: { name: string; _id: string };
 }
@@ -37,7 +42,7 @@ const useGetTransactionsByDate = (date: Date, timeSpan: string) => {
     transactionKeys.listByDate(formatDate),
     () =>
       axiosPrivate
-        .get<ITransaction[]>("/api/transactions", {
+        .get<ITransactionResponse[]>("/api/transactions", {
           params: { date: formatDate },
         })
         .then((res) => res.data),
@@ -64,7 +69,7 @@ const useGetTransactionsBudgetPreview = (budget?: IBudgetResponse) => {
     transactionKeys.listByFilters(searchString),
     () =>
       axiosPrivate
-        .get<ITransaction[]>(`/api/transactions?${searchString}`)
+        .get<ITransactionResponse[]>(`/api/transactions?${searchString}`)
         .then((res) => res.data),
     {
       enabled: budget && Object.keys(budget).length > 0,
@@ -90,7 +95,7 @@ const useGetFilteredTransactions = (currentPage: number) => {
     transactionKeys.listByFilters(searchString, currentPage),
     () =>
       axiosPrivate
-        .get<ITransaction[]>(`/api/transactions?${searchString}`, {
+        .get<ITransactionResponse[]>(`/api/transactions?${searchString}`, {
           params: {
             page: currentPage,
             limit: 10,
@@ -119,7 +124,7 @@ const useGetTransactionDetail = () => {
     transactionKeys.detail(id),
     () =>
       axiosPrivate
-        .get<ITransaction>(`/api/transactions/${id}`)
+        .get<ITransactionResponse>(`/api/transactions/${id}`)
         .then((res) => res.data),
     {
       staleTime: Infinity,
@@ -131,9 +136,98 @@ const useGetTransactionDetail = () => {
   );
 };
 
+const invalidateQueries = (
+  transaction: ITransactionResponse,
+  queryClient: QueryClient
+) => {
+  const transactionYear = new Date(transaction.date).getFullYear();
+  const transactionMonth = new Date(transaction.date).toLocaleDateString(
+    navigator.language,
+    { month: "long", year: "numeric" }
+  );
+
+  Promise.all([
+    queryClient.invalidateQueries(transactionKeys.listByDate(transactionYear)),
+    queryClient.invalidateQueries(transactionKeys.listByDate(transactionMonth)),
+    queryClient.invalidateQueries(transactionKeys.detail(transaction._id)),
+  ]);
+  if ((transaction.type = "expense")) {
+    Promise.all([
+      queryClient.invalidateQueries(
+        transactionKeys.listByFilters(
+          `type=expense&date=${transactionYear}&budget=${transaction.budget?.name}`
+        )
+      ),
+      queryClient.invalidateQueries(["budgets", transactionYear]),
+      queryClient.invalidateQueries(["budgets", transactionMonth]),
+      queryClient.invalidateQueries(["budgets", transaction.budget?._id]),
+    ]);
+  }
+};
+
+const useCreateNewTransaction = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading, isError, error, isSuccess } = useMutation(
+    (formData: ITransactionForm) =>
+      axiosPrivate
+        .post<ITransactionResponse>("/api/transactions", formData)
+        .then((res) => res.data)
+  );
+
+  const createNewTransaction = (formData: ITransactionForm) => {
+    return mutate(formData, {
+      onSuccess: (transaction) => invalidateQueries(transaction, queryClient),
+    });
+  };
+
+  return { createNewTransaction, isLoading, isError, error, isSuccess };
+};
+
+const useUpdateTransaction = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const { id } = useParams();
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading, isError, error, isSuccess } = useMutation(
+    (formData: ITransactionForm) =>
+      axiosPrivate
+        .put<ITransactionResponse>(`/api/transactions/${id}`, formData)
+        .then((res) => res.data)
+  );
+
+  const updateTransaction = (formData: ITransactionForm) => {
+    return mutate(formData, {
+      onSuccess: (transaction) => invalidateQueries(transaction, queryClient),
+    });
+  };
+
+  return { updateTransaction, isLoading, isError, isSuccess, error };
+};
+
+const useDeleteTransaction = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const queryClient = useQueryClient();
+  const { id } = useParams();
+
+  const { mutate: deleteTransaction } = useMutation(
+    () =>
+      axiosPrivate.delete(`/api/transactions/${id}`).then((res) => res.data),
+    {
+      onSuccess: (transaction) => invalidateQueries(transaction, queryClient),
+    }
+  );
+
+  return { deleteTransaction };
+};
+
 export {
   useGetTransactionsByDate,
   useGetFilteredTransactions,
   useGetTransactionsBudgetPreview,
   useGetTransactionDetail,
+  useCreateNewTransaction,
+  useUpdateTransaction,
+  useDeleteTransaction,
 };
