746aaba5e3667cd510c795791d9e6aa05ce3448f
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-16 21:55:03 +0100

    Update user model schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,14 +5,17 @@ const schema = mongoose.Schema;
 
 const { JWT_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
-const userSchema = new schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  userBudget: { type: Number, required: true },
-  password: { type: String, required: true },
-  currency: { type: String },
-});
+const userSchema = new schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    email: { type: String, required: true, unique: true },
+    userBudget: { type: Number, required: true },
+    password: { type: String, required: true },
+    currency: { type: String },
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
