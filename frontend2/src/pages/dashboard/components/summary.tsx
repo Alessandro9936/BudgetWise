ffb7636743d59775f2757ab6ef80f13b607ae5fc
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-21 21:18:57 +0100

    Refactor component UI and add dark theme styles

--- frontend2/src/pages/dashboard/components/summary.tsx
+++ frontend2/src/pages/dashboard/components/summary.tsx
@@ -1,5 +1,4 @@
-import CustomBarLoader from "../../../components/UI/bar-loader";
-import React from "react";
+import React, { useMemo } from "react";
 
 import TimeSpanButton from "../../../components/Buttons/TimeSpanButton";
 import Card from "../../../components/Utilities/card";
@@ -8,36 +7,29 @@ import useActiveDates from "../../../hooks/useActiveDates";
 import { useGetTransactionsByDate } from "../../../services/transaction-services";
 import { getCurrency } from "../../../context/user-context";
 
-const DateSelectorsContainer = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
-  return (
-    <div className="flex h-fit">
-      <div className="flex cursor-pointer items-center gap-1 rounded-full bg-white px-2 py-1 text-xs font-semibold text-neutral-500 shadow md:px-3 md:py-1">
-        <button>Sync dates</button>
-        <span>o</span>
-      </div>
-      <div className="ml-auto flex gap-2 md:gap-4">{children}</div>
-    </div>
-  );
-};
-
 const AmountContainer = ({
   children,
   label,
+  color,
 }: {
   children: React.ReactNode;
   label: string;
+  color: "red" | "green";
 }) => {
   const currency = getCurrency();
   return (
-    <Card classNames="px-4 py-3 flex-1 flex flex-col relative">
-      <p className=" mb-2 font-semibold">{label}</p>
-      <div className="my-auto flex h-fit items-center justify-between">
-        {children}
-        <span>{currency}</span>
+    <Card classNames="dark:bg-slate-800 flex-1 flex overflow-hidden">
+      <span
+        className={`mr-3 h-full w-2 transition-colors ${
+          color === "red" ? "bg-red-400" : "bg-green-400"
+        }`}
+      />
+      <div className="mr-5 flex-1 py-3">
+        <p className=" mb-2 font-semibold">{label}</p>
+        <div className="my-auto flex h-fit items-center justify-between">
+          {children}
+          <span>{currency}</span>
+        </div>
       </div>
     </Card>
   );
@@ -56,51 +48,60 @@ const Summary = ({ gridDisposition }: { gridDisposition: string }) => {
 
   const transactions = query.data ?? [];
 
-  const amounts = transactions.reduce(
-    (acc, cur) => {
-      cur.type === "income"
-        ? (acc.income += cur.amount)
-        : (acc.expenses += cur.amount);
-      return acc;
-    },
-    { income: 0, expenses: 0 }
+  const amounts = useMemo(
+    () =>
+      transactions.reduce(
+        (acc, cur) => {
+          cur.type === "income"
+            ? (acc.income += cur.amount)
+            : (acc.expenses += cur.amount);
+          return acc;
+        },
+        { income: 0, expenses: 0 }
+      ),
+    [transactions]
   );
 
   const totalBalance = amounts ? amounts.income - amounts.expenses : 0;
 
   return (
-    <section
-      className={`${gridDisposition} flex animate-appear flex-col gap-y-3`}
-    >
-      <DateSelectorsContainer>
-        {["Yearly", "Monthly"].map((timeSpan) => (
-          <TimeSpanButton
-            key={timeSpan}
-            timeSpan={timeSpan as "Yearly" | "Monthly"}
-            activeTimeSpan={activeTimeSpan}
-            updateActiveTimeSpan={updateActiveTimeSpan}
-          />
-        ))}
-      </DateSelectorsContainer>
+    <section className={`${gridDisposition} flex flex-col gap-y-4`}>
+      <div className="flex items-center justify-between">
+        <h3>Summary</h3>
+        <div className="ml-auto flex gap-2">
+          {["Yearly", "Monthly"].map((timeSpan) => (
+            <TimeSpanButton
+              key={timeSpan}
+              timeSpan={timeSpan as "Yearly" | "Monthly"}
+              activeTimeSpan={activeTimeSpan}
+              updateActiveTimeSpan={updateActiveTimeSpan}
+            />
+          ))}
+        </div>
+      </div>
+
       <DateBar
         updateActiveDate={updateActiveDate}
         activeDateFormatted={activeDateFormatted}
       />
-      <div className="flex flex-1 flex-col md:gap-y-4">
-        <AmountContainer label="Income">
+      <div className="flex flex-1 flex-col gap-y-4">
+        <AmountContainer label="Income" color="green">
           <p className="text-lg font-semibold md:text-2xl ">
             {amounts ? amounts.income : 0}
           </p>
         </AmountContainer>
-        <AmountContainer label="Expenses">
+        <AmountContainer label="Expenses" color="red">
           <p className="text-lg font-semibold md:text-2xl">
             {amounts ? amounts.expenses : 0}
           </p>
         </AmountContainer>
-        <AmountContainer label="Total">
+        <AmountContainer
+          label="Total"
+          color={totalBalance > 0 ? "green" : "red"}
+        >
           <p
             className={`text-lg font-semibold md:text-2xl ${
-              totalBalance > 0 ? "text-green-500" : "text-red-500"
+              totalBalance > 0 ? "text-green-400" : "text-red-400"
             }`}
           >
             {totalBalance}
