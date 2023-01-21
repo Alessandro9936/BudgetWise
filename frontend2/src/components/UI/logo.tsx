98d3acbfc1f28f89fad6a3745d4648d699731d98
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-21 21:18:05 +0100

    Extract logo

---
+++ frontend2/src/components/UI/logo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Logo = ({ redirect }: { redirect: string }) => {
+  return (
+    <Link to={redirect} className="text-2xl font-semibold">
+      Budget<span className="text-indigo-500">Wise</span>
+    </Link>
+  );
+};
+
+export default Logo;
