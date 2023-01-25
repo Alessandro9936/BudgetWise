622a194aebde892167cecb4d9117c5145b263ea4
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-25 21:13:47 +0100

    Update display of popup on hover, add animations

--- frontend2/src/pages/dashboard/components/calendar.tsx
+++ frontend2/src/pages/dashboard/components/calendar.tsx
@@ -7,42 +7,47 @@ import {
   useGetTransactionsByDate,
 } from "../../../services/transaction-services";
 import { useState } from "react";
-import { isSameDay } from "date-fns";
 import { getCurrency } from "../../../context/user-context";
 
+import { AnimatePresence, EventInfo, motion } from "framer-motion";
+
 interface ITransactionPopup {
-  coords: { top: string | undefined; left: string | undefined } | undefined;
+  coords: { top: string; left: string };
   transactions: ITransactionResponse[];
 }
 
 const TransactionPopup = ({ coords, transactions }: ITransactionPopup) => {
   const currency = getCurrency();
   return (
-    <ul
-      className={`pointer-events-none absolute z-10 flex w-40 origin-top-right animate-fadeIn flex-col gap-2 rounded-xl bg-purple-500 p-2 font-semibold text-white`}
-      style={{ top: coords?.top, left: coords?.left }}
-    >
-      {transactions.map((transaction) => (
-        <li key={transaction._id}>
-          <p>{transaction.description}</p>
-          <p>
-            Type: <span className="font-normal">{transaction.type}</span>
-          </p>
-          <p>
-            Amount:{" "}
-            <span className="font-normal">
-              {transaction.amount} {currency}
-            </span>
-          </p>
-          {transaction?.type === "expense" && (
-            <p>
-              Budget:{" "}
-              <span className="font-normal">{transaction.budget?.name}</span>
+    <AnimatePresence>
+      <motion.ul
+        initial={{ scale: 0, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1, transition: { duration: 0.2 } }}
+        className={`pointer-events-none absolute z-10 flex w-44 origin-top-right flex-col gap-y-3 rounded-xl bg-indigo-500 p-2 px-3 font-semibold text-white`}
+        style={{ top: coords?.top, left: coords?.left }}
+      >
+        {transactions.map((transaction) => (
+          <li key={transaction._id}>
+            <p>{transaction.description}</p>
+            <p className="text-sm">
+              Type: <span className="font-normal">{transaction.type}</span>
             </p>
-          )}
-        </li>
-      ))}
-    </ul>
+            <p className="text-sm">
+              Amount:{" "}
+              <span className="font-normal">
+                {transaction.amount} {currency}
+              </span>
+            </p>
+            {transaction?.type === "expense" && (
+              <p className="text-sm">
+                Budget:{" "}
+                <span className="font-normal">{transaction.budget?.name}</span>
+              </p>
+            )}
+          </li>
+        ))}
+      </motion.ul>
+    </AnimatePresence>
   );
 };
 
@@ -51,72 +56,70 @@ const Calendar = ({ gridDisposition }: { gridDisposition: string }) => {
 
   const [isPopupVisible, setIsPopupVisible] = useState(false);
   const [popupCoords, setPopupCoords] = useState<{
-    top: string | undefined;
-    left: string | undefined;
+    top: string;
+    left: string;
   }>();
+
   const [transactionsPopup, setTransactionsPopup] = useState<
     ITransactionResponse[]
   >([]);
 
-  const query = useGetTransactionsByDate(calendarDate, "Monthly");
+  const queryTransactions = useGetTransactionsByDate(calendarDate, "Monthly");
+  const transactions = queryTransactions.data ?? [];
 
-  const transactions = query.data ?? [];
-
-  const onPreviewHover = (
-    e: React.MouseEvent<HTMLSpanElement, MouseEvent>,
+  const showTransactions = (
+    info: EventInfo,
     transactionsOnDate: ITransactionResponse[]
   ) => {
     setPopupCoords({
-      top: e.clientY + 10 + "px",
-      left: e.clientX - 150 + "px",
+      top: info.point.y + 10 + "px",
+      left: info.point.x - 150 + "px",
     });
     setTransactionsPopup(transactionsOnDate);
   };
 
   return (
-    <section className={`${gridDisposition} flex flex-col gap-y-3`}>
-      <h3 className="text-base font-semibold">Calendar</h3>
+    <motion.section
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.75, type: "tween" }}
+      className={`${gridDisposition} flex flex-col gap-y-4`}
+    >
+      <h3 className="font-semibold">Calendar</h3>
       {isPopupVisible && (
         <TransactionPopup
-          coords={popupCoords}
+          coords={popupCoords!}
           transactions={transactionsPopup}
         />
       )}
-      <Card classNames="flex-1 py-2 px-4 animate-appear">
+
+      <Card classNames="dark:bg-slate-800 flex-1 py-2 px-4">
         <CalendarDep
           onActiveStartDateChange={({ activeStartDate }) =>
             setCalendarDate(activeStartDate)
           }
           minDetail="year"
           tileContent={({ date, view }) => {
-            const transactionsOnDate = transactions.filter(
+            const transactionsOnHoveredDate = transactions.filter(
               (transaction) =>
-                isSameDay(date, transaction.date) && view === "month"
+                date.getDate() === transaction.date.getDate() &&
+                view === "month"
             );
 
-            return transactionsOnDate.length > 0
-              ? transactionsOnDate.map((transaction) => {
-                  return (
-                    <span
-                      key={transaction._id}
-                      className={`absolute top-1 right-1 rounded-2xl p-1 ${
-                        transaction.type === "income"
-                          ? "bg-green-500"
-                          : "bg-red-500"
-                      }`}
-                      onMouseOver={(e) => {
-                        onPreviewHover(e, transactionsOnDate);
-                        setIsPopupVisible(true);
-                      }}
-                      onMouseOut={() => setIsPopupVisible(false)}
-                    />
-                  );
-                })
-              : null;
+            return transactionsOnHoveredDate.length > 0 ? (
+              <motion.span
+                className={`absolute top-1 right-1 rounded-full bg-indigo-500  p-1 hover:animate-pulse`}
+                onHoverStart={(_, info) => {
+                  showTransactions(info, transactionsOnHoveredDate);
+                  setIsPopupVisible(true);
+                }}
+                onHoverEnd={() => setIsPopupVisible(false)}
+              />
+            ) : null;
           }}
         />
       </Card>
-    </section>
+    </motion.section>
   );
 };
 export default Calendar;
