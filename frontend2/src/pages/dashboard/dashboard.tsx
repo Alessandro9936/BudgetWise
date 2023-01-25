b9e85c9a6f219e07adc41b13b0d8a4cfeeb011ad
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-25 21:10:32 +0100

    Implement loading handling before display content

--- frontend2/src/pages/dashboard/dashboard.tsx
+++ frontend2/src/pages/dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { useGetTransactionsByDate } from "../../services/transaction-services";
 import Activity from "./components/activity";
 import Calendar from "./components/calendar";
 import Graph from "./components/graph";
@@ -6,20 +7,29 @@ import Graph from "./components/graph";
 import Summary from "./components/summary";
 
 const Dashboard = () => {
+  const firstQuery = useGetTransactionsByDate(new Date(), "Monthly");
+  const isLoading = firstQuery.isLoading;
   return (
     <>
-      <section className="grid flex-1 grid-cols-1 gap-6 bg-gray-100 p-6 md:grid-cols-2 lg:grid-cols-[1.25fr_1fr_1.25fr] lg:grid-rows-[auto_1fr]">
-        {/* Graph */}
-        <Graph gridDisposition="order-2 md:col-span-full lg:order-1 lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-4 " />
+      <section className="grid flex-1 gap-6 bg-neutral-100 p-6 dark:bg-slate-900 md:grid-cols-2 lg:grid-cols-[1.25fr_1fr_1.25fr]  lg:grid-rows-[auto_1fr]">
+        {!isLoading ? (
+          <>
+            {/* Graph */}
+            <Graph gridDisposition="order-3 md:col-span-full md:order-2 lg:order-1 lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-4 " />
 
-        {/* Summary */}
-        <Summary gridDisposition="order-1 md:order-3" />
+            {/* Summary */}
 
-        {/* Calendar */}
-        <Calendar gridDisposition="order-3 md:order-4 " />
+            <Summary gridDisposition="order-1" />
 
-        {/* Activity */}
-        <Activity gridDisposition="order-4 col-span-full lg:order-2 lg:row-start-2 lg:row-end-4 lg:col-start-3 lg:col-end-5 lg:row-start-1" />
+            {/* Calendar */}
+            <Calendar gridDisposition="order-2 md:order-1" />
+
+            {/* Activity */}
+            <Activity gridDisposition="order-4 col-span-full lg:order-2 lg:row-start-2 lg:row-end-4 lg:col-start-3 lg:col-end-5 lg:row-start-1" />
+          </>
+        ) : (
+          <h3>Loading...</h3>
+        )}
       </section>
       <Outlet />
     </>
