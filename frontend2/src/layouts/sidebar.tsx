72eb00c67c82fd419494945564fd7f68d7ecc6da
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-30 21:22:39 +0100

    Add layout sharing and adjust UI

--- frontend2/src/layouts/sidebar.tsx
+++ frontend2/src/layouts/sidebar.tsx
@@ -1,140 +1,134 @@
-import {
-  BiCaretLeftCircle,
-  BiExit,
-  BiHomeAlt,
-  BiMenu,
-  BiPieChartAlt2,
-  BiTransfer,
-  BiUser,
-  BiX,
-} from "react-icons/bi";
+import { BiCaretLeftCircle, BiExit } from "react-icons/bi";
 
 import { motion } from "framer-motion";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import Logo from "../components/UI/logo";
 import ThemeToggle from "../components/Utilities/ThemeToggle";
 import { useLogoutUser } from "../services/user/user-services";
+import links from "./utils/sidebar-links";
+import { useState } from "react";
 
 const sidebarVariants = {
   closed: { width: "fit-content", transition: { type: "tween" } },
   open: {
-    width: 250,
+    width: 260,
     transition: {
       type: "tween",
-      duration: 0.15,
+      duration: 0.25,
+      delayChildren: 0.25,
       staggerChildren: 0.15,
     },
   },
 };
 
 const itemsVariants = {
-  closed: { opacity: 0, width: 0, y: "-25%" },
+  closed: { opacity: 0, y: "-25%", display: "none" },
   open: {
-    opacity: 1,
     y: 0,
+    opacity: 1,
+    display: "block",
+    transition: { type: "tween" },
   },
 };
 
 interface ISidebar {
   isMobile: boolean;
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Sidebar = ({ isMobile, isOpen, setIsOpen }: ISidebar) => {
-  const sidebarLinks = [
-    {
-      name: "Dashboard",
-      route: "/dashboard",
-      icon: <BiHomeAlt size={isMobile ? 20 : 24} />,
-    },
-    {
-      name: "Invoices",
-      route: "/invoices",
-      icon: <BiTransfer size={isMobile ? 20 : 24} />,
-    },
-    {
-      name: "Budgets",
-      route: "/budgets",
-      icon: <BiPieChartAlt2 size={isMobile ? 20 : 24} />,
-    },
-
-    {
-      name: "Profile",
-      route: "/profile",
-      icon: <BiUser size={isMobile ? 20 : 24} />,
-    },
-  ];
+const Sidebar = ({ isMobile }: ISidebar) => {
+  const [isOpen, setIsOpen] = useState(isMobile ? true : false);
+  const sidebarLinks = links(isMobile);
 
   const { logoutUser } = useLogoutUser();
 
+  const { pathname } = useLocation();
+  const [selectedRoute, setActiveRoute] = useState(pathname);
+  const [hoveredRoute, setHoveredRoute] = useState<string | null>(null);
+
   return (
-    <aside className="sticky top-0 z-20 flex h-screen max-w-fit flex-1 flex-col gap-y-10 bg-white py-6 pl-4 shadow-md dark:bg-gray-800">
-      <div className="flex h-7 items-center gap-x-4 pl-2 pr-4 md:pr-6">
-        <motion.div
-          variants={{
-            open: { rotate: 180 },
-            closed: { rotate: 0 },
-          }}
-          animate={isOpen ? "open" : "closed"}
-          transition={{ duration: 0.2 }}
-        >
-          <BiCaretLeftCircle
-            size={isMobile ? 20 : 24}
-            cursor={"pointer"}
-            onClick={() => setIsOpen(!isOpen)}
-          />
-        </motion.div>
-
-        {isOpen && (
-          <motion.span
-            variants={itemsVariants}
-            initial="closed"
-            animate={isOpen ? "open" : "closed"}
-          >
-            <Logo redirect="/dashboard" />
-          </motion.span>
-        )}
-      </div>
-
+    <aside className="sticky top-0 z-20 flex h-screen flex-col gap-y-10 bg-white py-6 pl-4 shadow-md dark:bg-gray-800">
       <motion.ul
+        onMouseLeave={() => setHoveredRoute(null)}
         variants={sidebarVariants}
         initial="closed"
         animate={isOpen ? "open" : "closed"}
         className="flex flex-1 flex-col gap-y-8"
       >
+        <div className="mb-4 flex h-7 items-center gap-x-4 pl-2 pr-4 md:pr-6">
+          <motion.div
+            variants={{
+              open: { rotate: 180 },
+              closed: { rotate: 0 },
+            }}
+            animate={isOpen ? "open" : "closed"}
+            transition={{ duration: 0.2 }}
+          >
+            <BiCaretLeftCircle
+              size={isMobile ? 20 : 24}
+              cursor={"pointer"}
+              onClick={() => setIsOpen(!isOpen)}
+            />
+          </motion.div>
+
+          {isOpen && (
+            <motion.span variants={itemsVariants}>
+              <Logo redirect="/dashboard" />
+            </motion.span>
+          )}
+        </div>
+
         {sidebarLinks.map((link) => (
-          <li key={link.name}>
+          <motion.li
+            className="relative"
+            key={link.route}
+            onClick={() => setActiveRoute(link.route)}
+            onHoverStart={() => setHoveredRoute(link.route)}
+          >
             <NavLink
               to={link.route}
               className={({ isActive }) =>
-                isActive
-                  ? "mr-4 flex items-center rounded-lg bg-indigo-500 p-2 font-semibold text-white transition dark:bg-indigo-600"
-                  : "mr-4 flex items-center rounded-lg p-2 focus:text-indigo-500 hover:text-indigo-500 hover:transition"
+                `mr-4 flex items-center rounded-lg p-2 ${
+                  isActive ? "font-semibold text-white" : ""
+                }`
               }
             >
-              {link.icon}
+              <span>{link.icon}</span>
               {isOpen && (
                 <motion.p className="ml-4" variants={itemsVariants}>
                   {link.name}
                 </motion.p>
               )}
+
+              {link.route === selectedRoute ? (
+                <motion.div
+                  layoutId="selected"
+                  className="absolute top-0 bottom-0 left-0 right-0 -z-10 mr-4 rounded-lg bg-indigo-500 dark:bg-indigo-600"
+                />
+              ) : null}
+              {link.route === hoveredRoute ? (
+                <motion.div
+                  layoutId="hovered"
+                  className="absolute top-0 bottom-0 left-0 right-0 -z-20 mr-4 rounded-lg bg-indigo-100 dark:bg-slate-700"
+                />
+              ) : null}
             </NavLink>
-          </li>
+          </motion.li>
         ))}
-        <ThemeToggle />
         <li
-          className="mr-4 flex cursor-pointer items-end  pl-2 hover:stroke-indigo-500 hover:text-indigo-500 hover:transition md:mt-auto"
+          className="mr-4 mt-auto flex cursor-pointer  items-end pl-2 hover:stroke-indigo-500 hover:text-indigo-500 hover:transition "
           onClick={() => logoutUser()}
         >
-          <BiExit size={isMobile ? 20 : 24} className="rotate-180" />
+          <span>
+            <BiExit size={isMobile ? 20 : 24} className="block rotate-180" />
+          </span>
           {isOpen && (
             <motion.p className="ml-4" variants={itemsVariants}>
               Logout
             </motion.p>
           )}
         </li>
+        <ThemeToggle isOpen={isOpen} />
       </motion.ul>
     </aside>
   );
