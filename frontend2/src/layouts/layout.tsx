c61fb98e756e87bf0ac0b4cc6f09ee9adc4abce5
Alessandro Crinella <alecrinella99@gmail.com>
2023-01-30 21:23:21 +0100

    Import sidebars and clean

--- frontend2/src/layouts/layout.tsx
+++ frontend2/src/layouts/layout.tsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import useCheckMobile from "../hooks/useCheckMobile";
 import Sidebar from "./sidebar";
 
+import SidebarMobile from "./sidebar-mobile";
+
 const Layout = () => {
   const { isMobile } = useCheckMobile();
-  const [isOpen, setIsOpen] = useState(isMobile ? true : false);
 
   return (
-    <main
-      className={`flex min-h-screen lg:h-screen ${
-        isOpen && isMobile ? "h-screen overflow-hidden" : ""
-      }`}
-    >
-      <Sidebar isMobile={isMobile} isOpen={isOpen} setIsOpen={setIsOpen} />
-      {isMobile && isOpen && (
-        <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex-1 bg-black/[.75]" />
+    <main className="flex min-h-screen lg:h-screen">
+      {!isMobile ? (
+        <Sidebar isMobile={isMobile} />
+      ) : (
+        <SidebarMobile isMobile={isMobile} />
       )}
+
       <Outlet />
     </main>
   );
