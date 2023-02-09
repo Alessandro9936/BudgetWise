6af96deca17ea00ff3c6d10b2cd107787fb49c48
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-09 20:35:05 +0100

    Add formatDate function

--- frontend2/src/services/format/date.ts
+++ frontend2/src/services/format/date.ts
@@ -1,10 +1,10 @@
-export const formatYear = (date: Date) =>
-  new Intl.DateTimeFormat(navigator.language, {
-    year: "numeric",
-  }).format(date);
-
 export const formatMonth = (date: Date) =>
   new Intl.DateTimeFormat(navigator.language, {
     month: "long",
     year: "numeric",
   }).format(date);
+
+export const formatDate = (date: Date) =>
+  new Intl.DateTimeFormat(navigator.language, { dateStyle: "long" }).format(
+    date
+  );
