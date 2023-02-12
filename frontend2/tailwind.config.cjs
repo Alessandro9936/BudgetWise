aa56a16ce8f3bc133f4d8637e29e219c1147a287
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-12 21:00:40 +0100

    Update tailwind config

--- frontend2/tailwind.config.cjs
+++ frontend2/tailwind.config.cjs
@@ -6,37 +6,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Montserrat"],
+        montserrat: ["Montserrat"],
       },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0%", transform: "scale(0%)" },
-          "100%": { opacity: "1", transform: "scale(100%)" },
-        },
-        appear: {
-          "0%": { opacity: "0%", translate: "0 10px" },
-          "100%": { opacity: "1", translate: "0" },
-        },
-        progressBar: {
-          "0%": { opacity: "50%", width: "0%" },
-          "100%": { opacity: "1", width: "100%" },
-        },
-      },
-      animation: {
-        fadeIn: "fadeIn 0.2s",
-        appear: "appear 0.3s",
-        progressBar: "progressBar 0.7s ease-in-out",
-      },
+
       gridTemplateColumns: {
         autoFill: "repeat(auto-fill, minmax(240px, 1fr))",
         autoFillBudgets: "repeat(auto-fill, minmax(280px, 1fr))",
+        autoFillBudgetOptions: "repeat(auto-fill, minmax(160px, 1fr))",
       },
     },
     screens: {
       sm: "360px",
-      midsm: "581px",
+      midsm: "608px",
       md: "768px",
-      lg: "1024px",
+      lg: "1080px",
       xl: "1440px",
     },
   },
