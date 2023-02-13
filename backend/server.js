fb45f05813a0931c50020f3b4733c9b4fcd99295
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-13 15:10:25 +0100

    Serve frontend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
@@ -24,12 +24,26 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+//app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Initiate routes
 app.use("/", userRoutes);
 app.use("/", transactionRoutes);
 app.use("/", budgetRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend2/dist")));
+
+  app.get("*", (_, res) =>
+    res.sendFile(
+      path.join(__dirname, "../frontend2/dist/index.html"),
+      function (err) {
+        res.status(500).send(err);
+      }
+    )
+  );
+}
 
 // Handle error if route not found (error 404) ot DB connection error
 app.use((req, res, next) => {
