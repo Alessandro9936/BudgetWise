23fe5d7d68461a3dfdced3144268bf627d3628b9
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-14 14:42:38 +0100

    Utilize manualChunks to split dependecies

--- frontend2/vite.config.ts
+++ frontend2/vite.config.ts
@@ -11,6 +11,31 @@ export default defineConfig({
     },
   },
 
+  build: {
+    // sets the warning threshold for chunk sizes
+    chunkSizeWarningLimit: 500,
+
+    //The manualChunks function in rollupOptions allows to specify how chunks should be split, so that the code from external libraries can be separated from the application code, resulting in smaller and more optimized chunks.
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            // ex. C:/project/node_modules/react-dom/index.js
+            return (
+              id
+                .toString()
+                // ex. "C:/project/node_modules/react-dom/index.js"
+                .split("node_modules/")[1]
+                // ex. ["C:/project/","react-dom/index.js"] => "react-dom/index.js"
+                .split("/")[0]
+                // ex. ["react-dom","index.js"] => "react-dom"
+                .toString()
+            );
+          }
+        },
+      },
+    },
+  },
   plugins: [react(), splitVendorChunkPlugin()],
 
   server: {
