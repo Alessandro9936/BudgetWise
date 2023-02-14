5d87f0905e7b85c1b69731c0ee2981a9f26dfacc
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-14 16:52:10 +0100

    Update cors

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({ origin: "https://budgetwise-three.vercel.app", credentials: true })
-);
+app.use(cors({ origin: "https://budgetwise.onrender.com", credentials: true }));
 
 // Initiate routes
 app.use("/", userRoutes);
