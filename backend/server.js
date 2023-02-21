820201fe70502c1391d3a4e757d5665c77f7ba8e
Alessandro Crinella <alecrinella99@gmail.com>
2023-02-21 06:39:10 +0100

    Implement helmet middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const createHttpError = require("http-errors");
 const credentials = require("./middlewares/credentials");
 const cors = require("cors");
+const helmet = require("helmet");
 const corsOptions = require("./config/corsOptions");
 const errorHandler = require("./middlewares/error-handler");
 const port = process.env.PORT || 8000;
@@ -23,11 +24,22 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
 
 // Initiate middlewares
 app.use(logger("dev"));
+
+// Parses incoming requests with JSON payloads
 app.use(express.json());
+
+// Parses incoming requests with urlencoded payloads
 app.use(express.urlencoded({ extended: true }));
+
+// Parse Cookie header and populate req.cookies with an objectr keyed by the cookie names
 app.use(cookieParser());
 
+// Collection of several smaller middleware functions that set security-related HTTP response headers
+app.use(helmet());
+
 app.use(credentials);
+
+// Allows web applications to make requests to resources from a different domain that the one the served the web page
 app.use(cors(corsOptions));
 
 // Initiate routes
